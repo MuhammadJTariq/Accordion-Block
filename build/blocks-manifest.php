9663b39dbc8e3e31aa2b02a2b8492b674205225e
Muhammad Tariq <muhammad.tws49@gmail.com>
2026-06-11 04:11:09 -0400

    First Commit

---
+++ build/blocks-manifest.php
@@ -0,0 +1,42 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'qa-accordion-block' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/qa-accordion-block',
+		'version' => '0.1.0',
+		'title' => 'Qa Accordion Block',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'Example block scaffolded with Create Block tool.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'qa-accordion-block',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'render' => 'file:./render.php',
+		'viewScript' => 'file:./view.js'
+	),
+	'qa-accordion-item' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/qa-accordion-item',
+		'title' => 'Accordion Item',
+		'category' => 'widgets',
+		'icon' => 'arrow-down-alt2',
+		'parent' => array(
+			'create-block/qa-accordion-block'
+		),
+		'editorScript' => 'file:./item.js',
+		'style' => 'file:./item-style.css',
+		'supports' => array(
+			'html' => false
+		)
+	)
+);
---
+++ build/qa-accordion-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '20342759890496e8894b');
